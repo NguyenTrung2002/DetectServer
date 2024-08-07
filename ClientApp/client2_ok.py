dc17fb08f9e03fcb722b295bab07b847e969e0ba
user <nguyenmanhtrung_t65@hus.edu.vn>
2024-08-07 16:28:54 +0700

    Thêm các chức năng, thêm các file code dùng để test, file code chính và file code mẫu 08/07/2024 4:30PM

--- ClientApp/client2_ok.py
+++ ClientApp/client2_ok.py
@@ -1,12 +1,9 @@
-#CODE HIỆN TẠI
+#CODE HOÀN THIỆN (LÀM MẪU)
 import socket
 import sys
+import threading
 from client_ui import Ui_MainWindow
 from PyQt5 import QtWidgets
-# from PyQt5 import QtCore, QtGui
-# from PyQt5.QtGui import QStandardItemModel, QStandardItem
-# import threading
-
 
 class Client:
     def __init__(self, host, port):
@@ -14,12 +11,10 @@ class Client:
         self.port = int(port)
         try:
             self.ip = socket.gethostbyname(self.host)
-            self.socket_client = socket.socket(socket.AF_INET,
-                                               socket.SOCK_STREAM)
+            self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.connected = False
         except:
             raise ValueError
-
 
     def start(self):
         try:
@@ -33,6 +28,18 @@ class Client:
         self.socket_client.close()
         print("Đã ngắt kết nối với server")
 
+    def receive_messages(self, callback):
+        while self.connected:
+            try:
+                data = self.socket_client.recv(1024)
+                if data:
+                    message = data.decode('utf-8')
+                    callback(message)
+                else:
+                    self.connected = False
+            except ConnectionResetError:
+                self.connected = False
+                break
 
 class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -46,7 +53,7 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.sendBut.setDisabled(True)
 
     def connect_server(self):
-        host=self.ipLine.text()
+        host = self.ipLine.text()
         port = self.portLine.text()
         if host == "" or port == "":
             self.chatTextEdit.append("Chưa nhập IP hoặc PORT")
@@ -61,17 +68,17 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.portLine.setDisabled(True)
             self.chatTextEdit.append(f"Client connected: "
                                      f"{self.client.socket_client.getsockname()[0]}:"
-                                     f" {self.client.socket_client.getsockname()[1]}"
-                                     )
+                                     f" {self.client.socket_client.getsockname()[1]}")
+            self.start_receiving()
         except ValueError:
             self.chatTextEdit.append("Sai địa chỉ IP")
         except ConnectionRefusedError:
-            self.chatTextEdit.append("Kết nối tới Server thất bại"
-                                     ", kiểm tra lại IP và PORT")
+            self.chatTextEdit.append("Kết nối tới Server thất bại, kiểm tra lại IP và PORT")
 
     def disconnect_server(self):
-        self.client.socket_client.close()
-        self.chatTextEdit.append(f"Client disconnect...")
+        if self.client:
+            self.client.close()
+            self.chatTextEdit.append("Client disconnected")
         self.connectBut.setDisabled(False)
         self.disconnectBut.setDisabled(True)
         self.sendBut.setDisabled(True)
@@ -80,23 +87,31 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def send_message(self):
         message = self.sendLine.text()
+        if message == '':
+            self.chatTextEdit.append("Vui lòng nhập tin nhắn")
+            return
         data = message.encode('utf-8')
         try:
             self.client.socket_client.sendall(data)
             self.chatTextEdit.append(f"Me: {message}")
             self.sendLine.clear()
         except ConnectionResetError:
-            self.chatTextEdit.append("Kết nối đã bị ngắt, "
-                                     "không thể gửi tin nhắn")
+            self.chatTextEdit.append("Kết nối đã bị ngắt, không thể gửi tin nhắn")
             self.connectBut.setDisabled(False)
             self.disconnectBut.setDisabled(True)
             self.sendBut.setDisabled(True)
             self.ipLine.setDisabled(False)
             self.portLine.setDisabled(False)
 
+    def start_receiving(self):
+        self.receive_thread = threading.Thread(target=self.client.receive_messages, args=(self.show_message,))
+        self.receive_thread.start()
+
+    def show_message(self, message):
+        self.chatTextEdit.append(f"Server: {message}")
+
     def get_message(self):
         pass
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
